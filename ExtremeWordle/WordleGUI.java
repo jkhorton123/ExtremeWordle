34fcb680bdf24ed05a3a3d3ff422e2913cd45328
jeh99 <jeh99@duke.edu>
2022-05-26 16:20:42 -0400

    Coded console-based wordle where the mystery word is set to hello by default

--- ExtremeWordle/WordleGUI.java
+++ ExtremeWordle/WordleGUI.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.util.Scanner;
 import java.util.*;
+
 class gui {
     public static void main(String args[]){
        JFrame frame = new JFrame("My First GUI");
@@ -10,38 +11,78 @@ class gui {
        frame.getContentPane().add(button); // Adds Button to content pane of frame
        frame.setVisible(true);
 
-       String wordleWord = "hello";
-       
+       String wordleWord = "hello".toLowerCase();
+       enteredWord word = new enteredWord();
+       word.outputWord(wordleWord);
+       System.exit(0);
 
     }
 }
 
 class enteredWord {
     public String guess;
-    
-    public void outputWord(String guess, String word) {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+    public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+    public Scanner wordScan = new Scanner(System.in);
+
+    public void outputWord(String word) {
         /*
         Prints the guess inputted by the user but highlights letters that match with the word with 
         green and highlights letters that are in the wrong position with yellow
         */
-        ArrayList<String> retList = new ArrayList<String>();
-        for (int i = 0; i<5; i++) {
-            Character guessChar = guess.charAt(i);
-            Character wordChar = word.charAt(i);
-            if (guessChar==wordChar) {
-                retList.add("\u002B31;1m"+Character.toString(guessChar));
+        String retList[] = new String[5];
+        int guessCount = 1;
+        while (guessCount <= 6) {
+            this.getWord();
+            int matches = 0;
+            for (int i = 0; i<5; i++) {
+                String guessLet = Character.toString(guess.charAt(i)).toLowerCase();
+                String wordLet = Character.toString(word.charAt(i)).toLowerCase();
+                if (guessLet.equals(wordLet)) {
+                    retList[i] = ANSI_GREEN_BACKGROUND+guessLet+ANSI_RESET;
+                    matches += 1;
+                }
+                else if (word.contains(guessLet)) {
+                    retList[i] = ANSI_YELLOW_BACKGROUND+guessLet+ANSI_RESET;
+                }
+                else {
+                    retList[i] = guessLet;
+                }
+            }
+        
+            for (int i = 0; i<5; i++) {
+                System.out.print(retList[i]);
+            }
+            System.out.println("");
+            guessCount+=1;
+            if (matches == 5){
+                System.out.println("You guessed the word correctly!");
+                guessCount = 8;
             }
         }
-        for (int i = 0; i<5; i++) {
-            System.out.println(retList.get(i));
+        if (guessCount == 7) {
+            System.out.println("Game Over, the correct word was " + word);
         }
+        wordScan.close();
     }
 
     public void getWord() {
-        Scanner wordScan = new Scanner(System.in);
-        System.out.println("Enter your guess: ");
-        guess = wordScan.nextLine();
-        wordScan.close();
+        boolean validWord = false;
+        while (!validWord) {
+            validWord = true;
+            System.out.println("Enter your guess: ");
+            guess = wordScan.nextLine();
+            if (!(guess.matches("[a-zA-Z]+"))) {
+                validWord = false;
+                System.out.println("Please enter a valid word");
+            }
+            else if (guess.length() != 5) {
+                validWord = false;
+                System.out.println("Entered word is not 5 letters");
+            }
+
+        }
     }
 }
 
